8396e1efdc9657def6b9a43d19af5f2248d06332
Kristopher Williams <kristopherwilliams@Kristophers-Mac-mini.local>
2021-12-03 23:35:56 -0600

    updated ssl

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,7 +5,6 @@ module.exports = {
     migrationPattern: path.join('migrations', '*'),
     driver: "pg",
     connectionString: process.env.DATABASE_URL,
-    /* "ssl": !!process.env.SSL, */
 }
 
 /* (process.env.NODE_ENV === 'test') ? process.env.TEST_DATABASE_URL :  */
